ae549024f92184a6bd69156d66d1fbe0b6903ed2
Steve Zatz <slzatz@gmail.com>
2021-08-12 13:45:51 -0400

    main: further progress - still rough but not far from first attempt to incorporate into listmango

--- main.go
+++ main.go
@@ -36,6 +36,12 @@ type JsonNotification struct {
 	Params  interface{} `json:"params"`
 }
 
+var jsonNotification = JsonNotification{
+	Jsonrpc: "2.0",
+	Method:  "initialize",
+	Params:  struct{}{},
+}
+
 func main() {
 	jsonRequest := JsonRequest{
 		Jsonrpc: "2.0",
@@ -44,11 +50,6 @@ func main() {
 		Params:  struct{}{},
 	}
 
-	jsonNotification := JsonNotification{
-		Jsonrpc: "2.0",
-		Method:  "initialize",
-		Params:  struct{}{},
-	}
 	params := protocol.InitializeParams{}
 	params.ProcessID = 0
 	params.RootURI = "file:///home/slzatz/go_fragments"
@@ -188,15 +189,17 @@ func main() {
 	fmt.Printf("Full Read = %s", fullRead)
 
 	z := make([]byte, 10000)
-	//i := 0
 	var j int32
 	fmt.Printf("\n\nEntering for loop\n\n")
+	diagnostics := make(chan []protocol.Diagnostic)
+	go receiveDiagnostics(diagnostics)
 	go func() {
+		// note if more than one jsonrpc message is read at one time; only dealing with first
+		var bb []byte
 		for {
 			n, err = buffer_out0.Read(z)
 			if err != nil {
 				log.Fatalf("\nRead -> %s\n%v", string(z), err)
-
 			}
 			fullRead = string(z)
 			fmt.Printf("Number of bytes read = %d\n", n)
@@ -205,19 +208,29 @@ func main() {
 			idx0 := bytes.Index(z, []byte(":")) + 2
 			idx := bytes.Index(z, []byte("\r\n\r\n"))
 			length, _ := strconv.Atoi(string(z[idx0:idx]))
-			bb := z[idx+4 : idx+4+length]
-			//idx = bytes.Index(bb, []byte("\x00"))
-			//fmt.Printf("\n\nIndex = %v\n\n", idx)
-			//var v protocol.InitializeResult
+
+			// not sure it's common but saw an instance where only got header info
+			if n < 30 {
+				fmt.Println("!!!Partial - Only got %q", fullRead[:n])
+				n, err = buffer_out0.Read(z)
+				if err != nil {
+					log.Fatalf("\nRead -> %s\n%v", string(z), err)
+				}
+				bb = append(bb, z[:n]...)
+			}
+
+			bb = z[idx+4 : idx+4+length]
 			var v JsonNotification
 			err = json.Unmarshal(bb, &v)
 			if err != nil {
 				log.Fatalf("\nA -> %s\n%v", string(bb), err)
 			}
 
-			fmt.Printf("\n\n-------------------------------\n\n")
-			fmt.Printf("params = %+v", v.Params)
-			fmt.Printf("\n\n-------------------------------\n\n")
+			/*
+				fmt.Printf("\n\n-------------------------------\n\n")
+				fmt.Printf("params = %+v", v.Params)
+				fmt.Printf("\n\n-------------------------------\n\n")
+			*/
 
 			if v.Method == "textDocument/publishDiagnostics" {
 				type JsonPubDiag struct {
@@ -228,102 +241,111 @@ func main() {
 				var vv JsonPubDiag
 				err = json.Unmarshal(bb, &vv)
 
-				fmt.Printf("\n\n-----------------------------------------------\n\n")
+				fmt.Printf("\n\n+++++++++++++++++++++++++++++++++++++++++++++++\n\n")
 				fmt.Printf("params = %+v\n", vv.Params)
 				fmt.Printf("uri = %+v\n", vv.Params.URI) //file:///home/slzatz/go_fragments/main.go
-				for i, d := range vv.Params.Diagnostics {
-					fmt.Printf("diagnostics = %+v\n", vv.Params.Diagnostics)
-					//fmt.Printf("diagnostics[0] = %+v\n", vv.Params.Diagnostics[0])
-					fmt.Printf("diagnostics[%d] = %+v\n", i, d)
-					//fmt.Printf("diagnostics[0].Range = %+v\n", vv.Params.Diagnostics[0].Range)                       //{Start:{Line:1 Character:0} End:{Line:1 Character:0}}
-					fmt.Printf("diagnostics[%d].Range = %+v\n", i, d.Range) //{Start:{Line:1 Character:0} End:{Line:1 Character:0}}
-					//fmt.Printf("diagnostics[0].Range.Start = %+v\n", vv.Params.Diagnostics[0].Range.Start)           //{Line:1 Character:0}
-					fmt.Printf("diagnostics[%d].Range.Start = %+v\n", i, d.Range.Start)           //{Line:1 Character:0}
-					fmt.Printf("diagnostics[%d].Range.Start.Line = %+v\n", i, d.Range.Start.Line) //1
-					//fmt.Printf("diagnostics[0].Range.Start.Line = %+v\n", vv.Params.Diagnostics[0].Range.Start.Line) //1
-				}
-				if len(vv.Params.Diagnostics) == 0 {
-					fmt.Println("Diagnostics was []")
-				}
-				fmt.Printf("\n\n-----------------------------------------------\n\n")
+				fmt.Printf("\n\n+++++++++++++++++++++++++++++++++++++++++++++++\n\n")
+				diagnostics <- vv.Params.Diagnostics
 			}
 
 			//time.Sleep(time.Second)
 		}
 	}()
-	for {
-		/*
-			i++
-			if (i % 10000000) == 0 {
-				fmt.Printf("i = %d\n", i)
-			}
-		*/
-		//fmt.Println(i)
-
-		//if false {
-		time.Sleep(time.Second * 4)
+	for i := 0; i < 2; i++ {
+		time.Sleep(time.Second * 2)
+		text := "package main\nimport \"fmt\"\n func main() {\n fmt.Println(\"hello\"\n}\n"
 		j++
-		jsonNotification.Method = "textDocument/didChange"
-		jsonNotification.Params = protocol.DidChangeTextDocumentParams{
-			TextDocument: protocol.VersionedTextDocumentIdentifier{
-				TextDocumentIdentifier: protocol.TextDocumentIdentifier{
-					URI: "file:///home/slzatz/go_fragments/main.go"},
-				Version: j},
-			ContentChanges: []protocol.TextDocumentContentChangeEvent{{Text: "package main\nimport \"fmt\"\n func main() {\n fmt.Println(\"hello\"\n}\n"}},
-		}
-		b, err = json.Marshal(jsonNotification)
-		if err != nil {
-			log.Fatalf("\n%s\n%v", string(b), err)
-		}
-		s = string(b)
-		header = fmt.Sprintf("Content-Length: %d\r\n\r\n", len(s))
-		s = header + s
-		fmt.Printf("\n\nSent incorrect: %s\n\n", s)
-		io.WriteString(stdin, s)
-		//time.Sleep(time.Second)
-		//if false {
-		time.Sleep(time.Second * 4)
-		//i = 0
+		sendDidChangeNotification(&stdin, text, j)
+
+		time.Sleep(time.Second * 2)
+		text = "package main\nimport \"fmt\"\n func main() {\n fmt.Println(\"hello\")\n}\n"
 		j++
-		jsonNotification.Method = "textDocument/didChange"
-		jsonNotification.Params = protocol.DidChangeTextDocumentParams{
-			TextDocument: protocol.VersionedTextDocumentIdentifier{
-				TextDocumentIdentifier: protocol.TextDocumentIdentifier{
-					URI: "file:///home/slzatz/go_fragments/main.go"},
-				Version: j},
-			ContentChanges: []protocol.TextDocumentContentChangeEvent{{Text: "package main\nimport \"fmt\"\n func main() {\n fmt.Println(\"hello\")\n}\n"}},
-		}
-		b, err = json.Marshal(jsonNotification)
-		if err != nil {
-			log.Fatalf("\n%s\n%v", string(b), err)
-		}
-		s = string(b)
-		header = fmt.Sprintf("Content-Length: %d\r\n\r\n", len(s))
-		s = header + s
-		fmt.Printf("\n\nSent corrected: %s\n\n", s)
-		io.WriteString(stdin, s)
-
-		/*
-				file := os.Stdout
-				fi, errr := file.Stat()
-				if errr != nil {
-					log.Fatalf("Couldn't stat file: %v", errr)
-				}
-				if fi.Size() == 0 {
-					//fmt.Println("Nothing to Read")
-					continue
-				}
-					_, err = buffer_out0.Peek(0)
-					if err != nil {
-						fmt.Println("Nothing to Read")
-						continue
-					}
-			xx := buffer_out0.Buffered()
-			if xx == 0 {
-				continue
-			}
-		*/
-
+		sendDidChangeNotification(&stdin, text, j)
 	}
+	// tell server the file is closed
+	jsonNotification.Method = "textDocument/didClose"
+	var closeParams protocol.DidCloseTextDocumentParams
+	closeParams.TextDocument.URI = "file:///home/slzatz/go_fragments/main.go"
+	jsonNotification.Params = closeParams
+	b, err = json.Marshal(jsonNotification)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s = string(b)
+	header = fmt.Sprintf("Content-Length: %d\r\n\r\n", len(s))
+	s = header + s
+	fmt.Printf("\n\n%s\n\n", s)
+	io.WriteString(stdin, s)
 
+	// shutdown request
+	shutdownRequest := JsonRequest{
+		Jsonrpc: "2.0",
+		Id:      2,
+		Method:  "shutdown",
+		Params:  nil,
+	}
+	b, err = json.Marshal(shutdownRequest)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s = string(b)
+	header = fmt.Sprintf("Content-Length: %d\r\n\r\n", len(s))
+	s = header + s
+	io.WriteString(stdin, s)
+	fmt.Printf("\n\n%s\n\n", s)
+
+	// exit notification
+	jsonNotification.Method = "exit"
+	jsonNotification.Params = nil
+	b, err = json.Marshal(jsonNotification)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s = string(b)
+	header = fmt.Sprintf("Content-Length: %d\r\n\r\n", len(s))
+	s = header + s
+	fmt.Printf("\n\n%s\n\n", s)
+	io.WriteString(stdin, s)
+
+	time.Sleep(10 * time.Second)
+}
+
+func sendDidChangeNotification(stdinp *io.WriteCloser, text string, j int32) {
+	jsonNotification.Method = "textDocument/didChange"
+	jsonNotification.Params = protocol.DidChangeTextDocumentParams{
+		TextDocument: protocol.VersionedTextDocumentIdentifier{
+			TextDocumentIdentifier: protocol.TextDocumentIdentifier{
+				URI: "file:///home/slzatz/go_fragments/main.go"},
+			Version: j},
+		ContentChanges: []protocol.TextDocumentContentChangeEvent{{Text: text}},
+	}
+	b, err := json.Marshal(jsonNotification)
+	if err != nil {
+		log.Fatalf("\n%s\n%v", string(b), err)
+	}
+	s := string(b)
+	header := fmt.Sprintf("Content-Length: %d\r\n\r\n", len(s))
+	s = header + s
+	fmt.Printf("\n\nSent incorrect: %s\n\n", s)
+	io.WriteString(*stdinp, s)
+
+}
+
+func receiveDiagnostics(diagnostics chan []protocol.Diagnostic) { //? []protocol.Diagnostics
+	for {
+		dd := <-diagnostics
+		fmt.Printf("\n\n-----------------------------------------------\n\n")
+		fmt.Printf("Diagnostics = %+v\n", dd)
+		for i, d := range dd {
+			fmt.Printf("Diagnostics = %+v\n", dd)
+			fmt.Printf("Diagnostics[%d] = %+v\n", i, d)
+			fmt.Printf("Diagnostics[%d].Range = %+v\n", i, d.Range)                       //{Start:{Line:1 Character:0} End:{Line:1 Character:0}}
+			fmt.Printf("Diagnostics[%d].Range.Start = %+v\n", i, d.Range.Start)           //{Line:1 Character:0}
+			fmt.Printf("Diagnostics[%d].Range.Start.Line = %+v\n", i, d.Range.Start.Line) //1
+		}
+		if len(dd) == 0 {
+			fmt.Println("Diagnostics was []")
+		}
+		fmt.Printf("\n\n-----------------------------------------------\n\n")
+	}
 }
